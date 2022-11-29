8c983cf24f8de45a77363d00a483231255d585f5
workday-possum-bayou <119368335+workday-possum-bayou@users.noreply.github.com>
2022-11-29 13:51:08 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function openCard() {
+    const envelope = document.querySelector('.envelope');
+
+    envelope.style.display = 'none';
+}
+
+function setVh() {
+    const root = document.documentElement;
+    const vh = window.innerHeight * 0.01;
+    root.style.setProperty('--vh', vh + 'px');
+}
+
+window.onload=function(){
+ setVh();
+window.addEventListener('resize', setVh);
+document.querySelector('.card__open-btn').addEventListener('click', openCard);
+  }
